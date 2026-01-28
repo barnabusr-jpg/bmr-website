1817da4b2d667855e40ef3db92d1858b15831bf4
barnabusr-jpg <barnabusr@gmail.com>
2026-01-28 05:18:49 -0500

    Updated routing to fix 404 error in the next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/downloads/HAI-AVS-Field-Guide-Summary',
+        destination: '/insights/field-guides',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
