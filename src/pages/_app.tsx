c644957684537d7e4c4d87b51fbe1e5a0823a5b1
barnabusr-jpg <barnabusr@gmail.com>
2026-02-02 14:20:19 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster"; // 1. Add this import
 
 const queryClient = new QueryClient();
 
@@ -12,7 +13,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <QueryClientProvider client={queryClient}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <TooltipProvider>
-            <Component {...pageProps} />;
+            <Component {...pageProps} />
+            <Toaster /> {/* 2. Add this here to enable confirmations */}
           </TooltipProvider>
         </ThemeProvider>
       </QueryClientProvider>
