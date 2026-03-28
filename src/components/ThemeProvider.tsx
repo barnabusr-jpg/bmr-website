fb4007a82c65d684f865c01facd11df8ac084c6b
barnabusr-jpg <barnabusr@gmail.com>
2026-03-28 13:27:00 -0400

    Update ThemeProvider.tsx

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,6 +1,23 @@
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes";
+"use client";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+/**
+ * BMR FORENSIC UNIT: STABLE THEME PROVIDER
+ * We've removed the explicit 'ThemeProviderProps' import to bypass 
+ * the library export discrepancy that is currently blocking the build.
+ */
+export function ThemeProvider({ 
+  children, 
+  ...props 
+}: { 
+  children: React.ReactNode; 
+  [key: string]: any 
+}) {
+  return (
+    <NextThemesProvider {...props}>
+      {children}
+    </NextThemesProvider>
+  );
 }
