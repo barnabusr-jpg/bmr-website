89593106e938cf6cbb2d0299d75d0268e1076327
barnabusr-jpg <barnabusr@gmail.com>
2026-03-30 10:55:10 -0400

    Delete src/additional-env.d.ts

--- src/additional-env.d.ts
+++
@@ -1,8 +0,0 @@
-import { MongoClient } from 'mongodb';
-
-declare global {
-  // This tells the editor that 'global._mongoClientPromise' is a valid path.
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
-}
-
-export {};
