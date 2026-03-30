b2cb740e117443374a43ebd3d8c60700e752795f
barnabusr-jpg <barnabusr@gmail.com>
2026-03-30 08:57:11 -0400

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -12,7 +12,7 @@ let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === 'development') {
   // In development, use a global variable to preserve the connection across HMR reloads.
-  let globalWithMongo = global as typeof globalThis & {
+  const globalWithMongo = global as typeof globalThis & {
     _mongoClientPromise?: Promise<MongoClient>;
   };
 
