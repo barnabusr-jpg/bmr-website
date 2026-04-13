04af40a346548a51235e38c5526189aa25e5a432
barnabusr-jpg <barnabusr@gmail.com>
2026-04-13 13:05:19 -0400

    Create forensic.tsx
    Import the component

---
+++ src/pages/diagnostic/forensic.tsx
@@ -0,0 +1,11 @@
+// src/pages/diagnostic/forensic.tsx
+import React from 'react';
+import ForensicDiagnostic from '@/components/ForensicDiagnostic'; 
+
+export default function ForensicPage() {
+  return (
+    <div className="min-h-screen bg-black">
+      <ForensicDiagnostic />
+    </div>
+  );
+}
