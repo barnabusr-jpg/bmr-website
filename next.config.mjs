24c21f252f91440796149e11557b5fd6c9675df7
barnabusr-jpg <barnabusr@gmail.com>
2026-07-29 11:09:02 -0400

    Update next.config.mjs
    Adding new button

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  serverExternalPackages: ["puppeteer-core", "@sparticuz/chromium"],
+  experimental: {
+    serverComponentsExternalPackages: ["puppeteer-core", "@sparticuz/chromium"],
+  },
 };
 
 export default nextConfig;
